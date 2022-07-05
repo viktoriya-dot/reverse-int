62fc753f3bf6ac4794b40e6d00fec88be2e1ac34
viktoriya-dot <201990kg@gmail.com>
2022-07-05 13:07:58 +0600

    finish reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
-}
+    let result = '';
+    if (n < 0) {
+      n *= -1
+    }
+    n = String(n).split('').reverse();
+      if (n[0] === '0') {
+        n.shift()
+      }
+    result = n.join('')
+    return result
+    }
+
